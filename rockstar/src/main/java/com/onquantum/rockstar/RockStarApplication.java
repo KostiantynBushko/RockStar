a95d5666edb114c29ef2c7fe9c9ecf6d19a39cc4
Bushko <bushko1979@gmail.com>
2015-07-26 02:36:12 +0300

    Override Application component

--- rockstar/src/main/java/com/onquantum/rockstar/RockStarApplication.java
+++ rockstar/src/main/java/com/onquantum/rockstar/RockStarApplication.java
@@ -2,6 +2,11 @@ package com.onquantum.rockstar;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import com.onquantum.rockstar.file_system.FileSystem;
+
+import java.io.File;
 
 /**
  * Created by Admin on 7/19/15.
@@ -14,6 +19,15 @@ public class RockStarApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+
+        File rootPath = new File(FileSystem.ROOT_PATH);
+        if(rootPath.exists() == false) {
+            rootPath.mkdir();
+        }
+        File iconPath = new File(FileSystem.ICON_PATH);
+        if(iconPath.exists() == false) {
+            iconPath.mkdir();
+        }
     }
 
     public static Context getContext() {
