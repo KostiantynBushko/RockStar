b1a2a0f430e64e012b7b4dadb133706f356d9d68
Bushko <bushko1979@gmail.com>
2015-08-08 01:28:13 +0300

    Available for switch between sound packages

--- rockstar/src/main/java/com/onquantum/rockstar/sequencer/QSoundPool.java
+++ rockstar/src/main/java/com/onquantum/rockstar/sequencer/QSoundPool.java
@@ -9,6 +9,9 @@ import android.os.Debug;
 import android.util.Log;
 
 import com.onquantum.rockstar.R;
+import com.onquantum.rockstar.file_system.FileSystem;
+import com.onquantum.rockstar.gsqlite.DBGuitarTable;
+import com.onquantum.rockstar.gsqlite.GuitarEntity;
 import com.onquantum.rockstar.midi.*;
 
 import com.onquantum.rockstar.Settings;
@@ -24,18 +27,16 @@ import java.util.List;
  * Created by Bushko on 12/20/14.
  */
 public class QSoundPool {
-    private static final QSoundPool instance = new QSoundPool();
+    private static QSoundPool instance = new QSoundPool();
     public static QSoundPool getInstance() {
         return instance;
     }
 
-    private String soundPath = "";
     private SoundPool soundPool;
-    private List<SoundPool> soundPools = new ArrayList<SoundPool>();
     private boolean loadedInProgress = false;
     private boolean successLoaded = false;
 
-    private String path = null;
+    //private String path = null;
     private String prefix;
 
     private Context context;
@@ -74,6 +75,9 @@ public class QSoundPool {
                 //Log.i("info"," QSoundPool : onGuitarPackageChange ");
             }
         });
+    }
+
+    private void Init() {
         soundPool = new SoundPool(200, AudioManager.STREAM_MUSIC,0);
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
             @Override
@@ -93,39 +97,41 @@ public class QSoundPool {
         });
     }
 
+
     public void setContext(Context context) {
         this.context = context;
     }
 
     private boolean stop = false;
     public void loadSound() {
+        GuitarEntity guitarEntity = DBGuitarTable.GetCurrentActive(context);
+        if(guitarEntity == null)
+            return;
+        Init();
         //Log.i("info","QSoundPool START LOAD SOUND" + soundPool.toString());
         loadSequenceNotes();
         if(successLoaded || loadedInProgress) {
-            //Log.i("info","  QSOUND POOL LOADED IN PROGRESS");
+            Log.i("info","  QSOUND POOL LOADED IN PROGRESS");
             return;
         }
-        prefix = new Settings(context).getCurrentGuitarPackage();
+        prefix = guitarEntity.article; //new Settings(context).getCurrentGuitarPackage();
 
         successLoaded = false;
         loadedInProgress = true;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 25; i++) {
-                    for (int j = 0; j < 6; j++){
-                        String file = prefix + "_" + Integer.toString(i) + "_" + Integer.toString(j);
-                        int id = context.getResources().getIdentifier(file,"raw",context.getPackageName());
-                        //Log.i("info"," FILE : " + file + " id = " + id);
-                        try {
-                            soundPool.load(context,id,1);
-                        }catch (Resources.NotFoundException e){
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }).start();
+        if(guitarEntity.id == 1) {
+            new Thread(new LoadSoundFromRes()).start();
+        } else {
+            new Thread(new LoadSoundFromFile()).start();
+        }
+    }
+
+    public void releaseSoundPool() {
+        soundPool.release();
+        soundPool = null;
+        loadedInProgress = false;
+        successLoaded = false;
+        load = 0;
+        loadProgress = 0;
     }
 
     public boolean isSuccessLoaded() {
@@ -133,7 +139,7 @@ public class QSoundPool {
     }
 
     public SoundPool getSoundPool() {
-        //Log.i("info","QSoundPool GET SOUND POOL " + soundPool.toString());
+        Log.i("info","QSoundPool GET SOUND POOL " + soundPool.toString());
         return soundPool;
     }
     public int getSuccessLoad() {
@@ -141,9 +147,6 @@ public class QSoundPool {
     }
     public int getLoadProgress() {
         return loadProgress;
-    }
-
-    public void Destroy() {
     }
 
     private void loadSequenceNotes() {
@@ -164,5 +167,52 @@ public class QSoundPool {
     }
     public int GetOctaveForGuitarString(int fretBar, int string) {
         return QMIDI.getOctaveForNote(tracks[0].get((string) + (6 * fretBar)).getMessage().getDataByteOne());
+    }
+
+    private class LoadSoundFromRes implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 25; i++) {
+                for (int j = 0; j < 6; j++){
+                    String file = prefix + "_" + Integer.toString(i) + "_" + Integer.toString(j);
+                    int id = context.getResources().getIdentifier(file,"raw",context.getPackageName());
+                    Log.i("info"," FILE : " + file + " id = " + id);
+                    try {
+                        synchronized (QSoundPool.this) {
+                            if(loadedInProgress) {
+                                soundPool.load(context,id,1);
+                            } else {
+                                Log.i("info","  ************** BRAKE LOADING ID = " + prefix);
+                                return;
+                            }
+                        }
+                    }catch (Resources.NotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    private class LoadSoundFromFile implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 25; i++) {
+                for (int j = 0; j < 6; j++){
+                    String file = prefix + "_" + Integer.toString(i) + "_" + Integer.toString(j);
+                    File filePath = new File(FileSystem.GetSoundFilesPath(prefix) + "/" + file + ".ogg") ;
+                    Log.i("info"," FILE : " + filePath.getAbsolutePath());
+                    try {
+                        if(loadedInProgress) {
+                            soundPool.load(filePath.getAbsolutePath(),1);
+                        } else {
+                            Log.i("info","  ************** BRAKE LOADING FILE = " + file);
+                            return;
+                        }
+                    }catch (Resources.NotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
