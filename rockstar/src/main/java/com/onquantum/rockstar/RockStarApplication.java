18aeb77ffdf7afd878971636a617688a7f864500
Bushko <bushko1979@gmail.com>
2015-09-09 22:05:44 +0300

    Run services at start

--- rockstar/src/main/java/com/onquantum/rockstar/RockStarApplication.java
+++ rockstar/src/main/java/com/onquantum/rockstar/RockStarApplication.java
@@ -2,9 +2,12 @@ package com.onquantum.rockstar;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import com.onquantum.rockstar.file_system.FileSystem;
+import com.onquantum.rockstar.services.UpdateGuitarsService;
+import com.onquantum.rockstar.services.UpdatePurchaseTable;
 
 import java.io.File;
 
@@ -22,6 +25,9 @@ public class RockStarApplication extends Application {
 
         FileSystem.GetRootPath();
         FileSystem.GetIconPath();
+
+        startService(new Intent(context, UpdateGuitarsService.class));
+        startService(new Intent(context, UpdatePurchaseTable.class));
     }
 
     public static Context getContext() {
